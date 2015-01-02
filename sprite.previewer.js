a0ecbc8522f4180d41b2099a6bdef65867ba0de1
dvberkel <daan.v.berkel.1980@gmail.com>
2015-01-02 08:36:06 +0100

    show a black square

--- sprite.previewer.js
+++ sprite.previewer.js
@@ -2,6 +2,13 @@
     var preview = sprite.preview = {};
 
     var View = preview.View = function(model, preview){
-        /* do nothing for the moment */
+        this.model = model;
+        this.preview = preview;
+	this.context = preview.getContext('2d');
+	this.model.on('paint', this.update.bind(this));
+	this.update();
     };
+    View.prototype.update = function(){
+	this.context.fillRect(0, 0, this.preview.width, this.preview.height);
+    }
 })(window.sprite = window.sprite || {})
