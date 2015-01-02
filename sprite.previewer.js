5a7b29af12cef6560a36b516825a7920623df054
dvberkel <daan.v.berkel.1980@gmail.com>
2015-01-02 08:59:04 +0100

    paint pixels in black

--- sprite.previewer.js
+++ sprite.previewer.js
@@ -4,16 +4,24 @@
     var View = preview.View = function(model, preview){
         this.model = model;
         this.preview = preview;
-	this.model.on('paint', this.update.bind(this));
 	this.initialize();
+	this.model.on('paint', this.paintPixel.bind(this));
 	this.update();
     };
     View.prototype.initialize = function(){
 	this.preview.width = this.model.columns;
 	this.preview.height = this.model.rows;
 	this.context = this.preview.getContext('2d');
+	this.imageData = this.context.createImageData(this.preview.width, this.preview.height);
     };
     View.prototype.update = function(){
-	this.context.fillRect(0, 0, this.preview.width, this.preview.height);
+	this.context.putImageData(this.imageData, 0, 0);
+    };
+    View.prototype.paintPixel = function(x, y, color){
+	var baseIndex = 4 * (this.model.columns * y + x);
+	[0, 0, 0, 255].forEach(function(value, index){
+	    this.data[baseIndex + index] = value;
+	}.bind(this.imageData));
+	this.update();
     };
 })(window.sprite = window.sprite || {})
