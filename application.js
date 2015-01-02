267b41ac4f74a639fa957e3645cfadae583d4c5c
dvberkel <daan.v.berkel.1980@gmail.com>
2015-01-02 22:13:40 +0100

    use application correctly

--- application.js
+++ application.js
@@ -3,9 +3,11 @@
 
 	var canvas = document.getElementById('pixel-editor');
 	var view = new sprite.editor.View(model, canvas);
+	var controller = sprite.editor.controllerFor(model, view, canvas);
 
 	var preview  = document.getElementById('pixel-preview');
 	var previewView = new sprite.preview.View(model, preview);
 
-	canvas.addEventListener('mousedown', sprite.editor.controllerFor(model, view));
+	canvas.addEventListener('mousedown', controller.startDrawing.bind(controller));
+	canvas.addEventListener('mouseup', controller.stopDrawing.bind(controller));
 })(sprite);
