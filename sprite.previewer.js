88db2f9c6e1dbb8b52c281e108eaa3026861b1ad
dvberkel <daan.v.berkel.1980@gmail.com>
2015-01-02 08:38:40 +0100

    let the preview match the sprite

--- sprite.previewer.js
+++ sprite.previewer.js
@@ -4,11 +4,16 @@
     var View = preview.View = function(model, preview){
         this.model = model;
         this.preview = preview;
-	this.context = preview.getContext('2d');
 	this.model.on('paint', this.update.bind(this));
+	this.initialize();
 	this.update();
+    };
+    View.prototype.initialize = function(){
+	this.preview.width = this.model.columns;
+	this.preview.height = this.model.rows;
+	this.context = this.preview.getContext('2d');
     };
     View.prototype.update = function(){
 	this.context.fillRect(0, 0, this.preview.width, this.preview.height);
-    }
+    };
 })(window.sprite = window.sprite || {})
